8cc2064626358e1e6a2b1f4a71d265233eb91fa5
EdgarGallardo <31519841+EdgarGallardo@users.noreply.github.com>
2017-10-26 13:25:42 -0500

    tarea 14
    tarea 14

---
+++ Tarea 14/variable y constante.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#define MAX 5 
+
+
+int main (int argc, char *argv[])
+{
+	int edad=0;
+	const float PI=3.1416;
+	
+	printf("cual es tu edad?:");
+	scanf("%d",& edad);
+
+//MAX=6;
+//PI=3.1416;
+
+
+printf("tu edad es=%d/n ", edad);
+printf("Maximo=%d",MAX);
+printf("valor de PI=%f", PI);
+
+
+return 0;
+	
+}
