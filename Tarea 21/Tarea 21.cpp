3357e4ee9c753742a6bc4875bcd43a58f57f68c2
EdgarGallardo <31519841+EdgarGallardo@users.noreply.github.com>
2017-11-16 23:28:25 -0600

    Tarea 21
    Tarea 21

---
+++ Tarea 21/Tarea 21.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+
+void appInfoData(char *titulo,char *fecha)
+{
+  printf("EEEEEE\n");
+  printf("_\n");
+    printf("EE\n");
+     printf("_\n");
+  printf("_   EE    %s\n", titulo);
+  printf("_       Edgar Ivan Salinas Gallardo ®\n");
+  printf("_       %s\n", fecha);
+  printf("_\n");
+  printf("****\n");
+}
